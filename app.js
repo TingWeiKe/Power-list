d108e959b977aba62a2b84b5c337c9d77966412c
TingWeiKe <rockmanx673@gmail.com>
2018-12-24 17:08:05 +0800

    first commit with complete Token Refreshing

---
+++ app.js
@@ -0,0 +1,39 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require('body-parser')
+var postRouter = require('./routes/post');
+var app = express();
+
+// view engine setup
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/post', postRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send('error');
+});
+
+module.exports = app;
---
+++ routes/post.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var router = express.Router();
+let axios = require('axios')
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+let grant_type = 'authorization_code'
+let code = ''
+var request = require("request");
+var qs = require("querystring");
+var http = require("https");
+var Qs = require('qs');
+
+
+router.post('/refresh', function (req, res, next) {
+  
+
+  let code = req.body.code
+ 
+  // POST access_token from KKbox
+  async function get_refresh_access_data() {
+    
+     //FormData must be a String in order to fit in content-type: application/x-www-form-urlencoded
+     //Use Qs.stringify
+    let data = Qs.stringify({
+      grant_type: 'refresh_token',
+      refresh_token: code,
+      client_id: 'b89dc89b34b7f4d2759580c9b53141ae',
+      client_secret: 'c47d5eebae5d6cf9da082e55447d4ec8'
+    })
+  
+    let config = {
+      method: 'post', url: 'https://account.kkbox.com/oauth2/token',
+      headers: { 'host': 'account.kkbox.com', 'content-type': 'application/x-www-form-urlencoded' },
+      // Must be a String in content-type: application/x-www-form-urlencoded
+      // Otherwise Axios will FUCKING JSON IT !!!
+      data: data
+    }
+    const res = await axios(config)
+    return res.data
+  }
+
+  get_refresh_access_data()
+    .then(data => {
+      console.log(data)
+      res.json(data)
+    }).catch(error=>{
+      res.json(error)
+    })
+});
+
+
+
+
+router.post('/', function (req, res, next) {
+  /* POST access_token from KKbox */
+  async function get_access_data(grant_type, code) {
+    //FormData must be a String in in content-type: application/x-www-form-urlencoded prevent Axios JSON it again.
+    //Use raw String
+    let formData = 'grant_type=' + grant_type + '&code=' + req.body.urlPara + '&client_id=b89dc89b34b7f4d2759580c9b53141ae&client_secret=c47d5eebae5d6cf9da082e55447d4ec8'
+    let config = {
+      method: 'post', url: 'https://account.kkbox.com/oauth2/token',
+      headers: { 'host': 'account.kkbox.com', 'content-type': 'application/x-www-form-urlencoded' },
+      // FormData FormData must be a String /
+      data: formData
+    }
+    const res = await axios(config)
+    return res.data
+  }
+
+  get_access_data(grant_type, code)
+    .then(data => {
+      data.access_token ? res.send(data) : res.send('No access_token')
+    }).catch(error => {
+      res.send(error)
+    }
+    )
+});
+
+module.exports = router;
